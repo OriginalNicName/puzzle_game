0244efc380eb4e4fc6b0faa3988164d91ddba704
OriginalNicName <55702773+OriginalNicName@users.noreply.github.com>
2020-03-11 09:55:28 +0000

    Add files via upload

---
+++ puzzle-game/assets/game/puzzle-tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="puzzle-tileset" tilewidth="64" tileheight="64" spacing="2" margin="1" tilecount="72" columns="8">
+ <image source="puzzle-tileset.png" width="528" height="594"/>
+</tileset>
--- puzzle-game/js/PuzzleScene.js
+++ puzzle-game/js/PuzzleScene.js
@@ -6,22 +6,36 @@ class PuzzleScene extends Phaser.Scene {
     wallLayer;
     interactLayer;
     exitLayer;
+    uiScene;
     constructor() {
         super("GameScene");
     }
 
+    init(data) {
+        this.music = new AudioManager(this);
+        this.sfx = new AudioManager(this);
+
+        if (data.music) {
+            this.music.volume = data.music.volume;
+            this.music.muted = data.music.muted;
+        }
+        if (data.sfx) {
+            this.sfx.volume = data.sfx.volume;
+            this.sfx.muted = data.sfx.muted;
+        }
+    }
 
     preload() {
-        this.load.image('tileset', 'assets/puzzle-tileset.png');
-        this.load.image('player', 'assets/player.png', {
+        this.load.image('tileset', 'assets/game/puzzle-tileset.png');
+        this.load.image('player', 'assets/game/player.png', {
             frameWidth: 24,
             frameHeight: 24
         });
-        this.load.image('left-block', 'assets/left-block.png')
-        this.load.image('right-block', 'assets/right-block.png')
-        this.load.image('up-block', 'assets/up-block.png')
-        this.load.image('down-block', 'assets/down-block.png')
-        this.load.tilemapTiledJSON('tilemap', 'assets/testmap.json');
+        this.load.image('left-block', 'assets/game/left-block.png')
+        this.load.image('right-block', 'assets/game/right-block.png')
+        this.load.image('up-block', 'assets/game/up-block.png')
+        this.load.image('down-block', 'assets/game/down-block.png')
+        this.load.tilemapTiledJSON('tilemap', 'assets/game/testmap.json');
     }
 
     create() {
@@ -52,9 +66,9 @@ class PuzzleScene extends Phaser.Scene {
                 this.createLeftBlock(object);
             } else if (object.type === "rightBlock") {
                 this.createRightBlock(object);
-            }else if (object.type === "upBlock") {
+            } else if (object.type === "upBlock") {
                 this.createUpBlock(object);
-            }else if (object.type === "downBlock") {
+            } else if (object.type === "downBlock") {
                 this.createDownBlock(object);
             }
         }, this);
@@ -62,6 +76,13 @@ class PuzzleScene extends Phaser.Scene {
         this.camera = this.cameras.getCamera("");
         this.camera.setBounds(0, 0, this.map.widthInPixels, this.map.height * this.map.tileHeight);
         this.createCollision();
+
+        this.uiScene = this.scene.get("UIScene");
+        this.scene.launch(this.uiScene);
+        this.uiScene.createUIElements(this);
+        this.music.addAudio('gameMusic', { loop: true });
+        this.music.play('gameMusic');
+        this.sfx.addAudio('success')
     }
 
     createPlayer(object) {
@@ -84,7 +105,7 @@ class PuzzleScene extends Phaser.Scene {
 
     createDownBlock(object) {
         this.downBlocks.create(object.x, object.y, 'down-block')
-    }    
+    }
 
     createCollision() {
         let wallLayer = this.map.getLayer('wall').tilemapLayer;
@@ -103,23 +124,23 @@ class PuzzleScene extends Phaser.Scene {
     }
 
     turnLeft(player) {
-       this.player.setAngle(180);
-       this.physics.velocityFromRotation(player.rotation, 300, player.body.velocity);
+        this.player.setAngle(180);
+        this.physics.velocityFromRotation(player.rotation, 300, player.body.velocity);
     }
 
     turnRight(player) {
-       this.player.setAngle(0);
-       this.physics.velocityFromRotation(player.rotation, 300, player.body.velocity);
+        this.player.setAngle(0);
+        this.physics.velocityFromRotation(player.rotation, 300, player.body.velocity);
     }
 
     goUp(player) {
-       this.player.setAngle(270);
-       this.physics.velocityFromRotation(player.rotation, 300, player.body.velocity);
+        this.player.setAngle(270);
+        this.physics.velocityFromRotation(player.rotation, 300, player.body.velocity);
     }
 
-    goDown(player){
-       this.player.setAngle(90);
-       this.physics.velocityFromRotation(player.rotation, 300, player.body.velocity);
+    goDown(player) {
+        this.player.setAngle(90);
+        this.physics.velocityFromRotation(player.rotation, 300, player.body.velocity);
     }
 
     resetPlayer() {
@@ -138,10 +159,8 @@ class PuzzleScene extends Phaser.Scene {
                     object[propName] = object.properties[propName]; //Create the property in the object
                 }
             }
-
             delete object.properties; //Delete the custom properties array from the object
         }
-
         return object; //Return the new object w/ custom properties
     }
 
@@ -161,8 +180,8 @@ class PuzzleScene extends Phaser.Scene {
     }
 
     processExit() {
+        this.sfx.play('success');
         this.physics.pause();
-        console.log("goal achieved");
     }
 
 }
@@ -174,5 +193,125 @@ class UIScene extends Phaser.Scene {
 
     createUIElements(gameScene) {
         this.gameScene = gameScene;
+
+        this.playMenu = new Menu(this, 705, 0, 195, 1350, 'menuBox', [
+            new Button(this, 10, 100, "playButton", function () {
+                this.scene.gameScene.body.movePlayer();
+            }),
+        ]);
+        this.add.existing(this.playMenu);
+        this.playMenu.setVisible(true);
+    }
+}
+
+class MenuScene extends Phaser.Scene {
+    constructor() {
+        super('MenuScene');
+    }
+
+    init(data) {
+        this.music = new AudioManager(this);
+        this.sfx = new AudioManager(this);
+
+        if (data.music) {
+            this.music.volume = data.music.volume;
+            this.music.muted = data.music.muted;
+        }
+
+        if (data.sfx) {
+            this.sfx.volume = data.sfx.volume;
+            this.sfx.muted = data.sfx.muted;
+        }
+    }
+
+    preload() {
+        this.load.image('homeButton', 'assets/ui/home.png');
+        this.load.image('menuButton', 'assets/ui/menu.png');
+        this.load.image('crossButton', 'assets/ui/cross.png');
+        this.load.image('muteButton', 'assets/ui/mute.png');
+        this.load.image('playButton', 'assets/ui/play.png');
+        this.load.image('resetButton', 'assets/ui/reset.png');
+        this.load.image('settingsButton', 'assets/ui/settings.png');
+        this.load.image('tickButton', 'assets/ui/tick.png');
+        this.load.image('unmuteButton', 'assets/ui/unmute.png');
+        this.load.image('volumeButton', 'assets/ui/volume.png');
+        this.load.image('menuBox', 'assets/ui/menuBox.png');
+        this.load.image('sliderBar', 'assets/ui/slider-bar.png');
+        this.load.image('sliderDial', 'assets/ui/slider-dial.png');
+        this.load.image('sliderOutline', 'assets/ui/slider-outline.png');
+
+        this.load.audio("menuMusic", "assets/audio/menu-[AudioTrimmer.com].wav");
+        this.load.audio("gameMusic", "assets/audio/promiso8bit-[AudioTrimmer.com].wav");
+        this.load.audio("success", "assets/audio/success.wav");
+    }
+
+    create() {
+        this.mainMenu = new Menu(this, 30, 30, config.width - 30, config.height - 20, 'menuBox', [
+            this.titleText = this.add.text(100, 40, "Critter Escape", {
+                font: '100px Forte',
+                fill: '#d6d1a1'
+            }),
+            new Button(this, 30, 150, 'playButton', function () {
+                this.scene.music.stopAll();
+                this.scene.scene.start("GameScene", {
+                    music: this.scene.music
+                })
+            }), this.menuText = this.add.text(240, 220, "Play", {
+                font: '64px Forte',
+                fill: '#d6d1a1'
+            }),
+            new Button(this, 27, 350, 'settingsButton', function () {
+                this.scene.settingMenu.setVisible(true);
+                this.scene.mainMenu.setVisible(false);
+            }),
+            this.menuText = this.add.text(240, 410, "Options", {
+                font: '64px Forte',
+                fill: '#d6d1a1'
+            })
+        ]);
+        this.add.existing(this.mainMenu);
+        this.settingMenu = new Menu(this, 30, 30, config.width - 30, config.height - 20, 'menuBox', [
+            this.titleText = this.add.text(220, 40, "Options", {
+                font: '100px Forte',
+                fill: '#d6d1a1'
+            }),
+            new Button(this, 30, 150, 'homeButton', function () {
+                this.scene.settingMenu.visible = false;
+                this.scene.mainMenu.visible = true;
+            }),
+            this.menuText = this.add.text(240, 220, "Home", {
+                font: '64px Forte',
+                fill: '#d6d1a1'
+            }),
+            new Button(this, 30, 350, 'muteButton', function () {
+                this.scene.music.toggleMute();
+            }, true, true, !this.music.muted, 'unmuteButton'),
+            this.menuText = this.add.text(240, 410, "Mute", {
+                font: '64px Forte',
+                fill: '#d6d1a1'
+            }),
+            // new Slider(this, 40, 600, 450, 60, "sliderOutline", "sliderDial", function () {
+            //     this.scene.music.setVolume(this.percent / 100);
+            // }),
+            this.menuText = this.add.text(530, 600, "Volume", {
+                font: '60px Forte',
+                fill: '#d6d1a1'
+            }),
+            new MaskSlider(this, 40, 600, 450, 60, "sliderOutline", "sliderBar", "sliderDial", function () {
+                this.scene.music.setVolume(this.percent / 100);
+                this.scene.sfx.setVolume(this.percent / 100);
+            }),
+        ]);
+
+        this.add.existing(this.settingMenu);
+        this.settingMenu.setVisible(false)
+
+        this.input.on('pointerup', function (pointer) {
+            if (pointer.lastBtn) {
+                pointer.lastBtn.clearTint();
+            }
+        });
+        this.music.addAudio('menuMusic', { loop: true });
+        this.music.play('menuMusic');
     }
 }
